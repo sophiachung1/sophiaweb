1b06d921889a72d600f4403d97f2453504798040
chungsop <sxhchung@gmail.com>
2025-03-10 22:24:28 -0500

    fixed merge conflics

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ export default {
   server: {
     port: 3001,  // You can change this to another port number
   },
+  build: {
+    outDir: 'dist', // this specifies the output directory as 'dist'
+  },
 };
